607b341fac8065ad9bb38fd7ae8a4677fbc784db
pushpanjay2 <57862635+pushpanjay2@users.noreply.github.com>
2024-01-16 18:14:19 +0530

    Update app.py
    New route addes

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def main():
 def hello():
     return 'I am good, how about you?'
 
+@app.route('/expression/')
+def exper()
+return 'Go with new route'
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
