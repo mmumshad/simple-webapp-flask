d49af0159426d625156343392cdc4679f6952091
tapanunix <tapanunix@outlook.com>
2024-05-22 23:55:25 +0530

    message

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "OMM NAMHA SIVAYA!"
+    return "OMM NAMHA Ganapati!"
 
 @app.route('/how are you')
 def hello():
-    return 'BABA AKHANDLAMANI?'
+    return 'BABA Sivaya'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
